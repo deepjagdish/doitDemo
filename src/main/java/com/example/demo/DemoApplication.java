bcdf1449b822efa0f6c964560171773837101022
deepjagdish <deepjagdish@gmail.com>
2022-06-03 10:17:55 +0530

    Update DemoApplication.java
    change text

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World DoIT on 3rd June!";
+        return "Hello class, Welcome to OpenShift Demo!";
     }
 
     public static void main(String[] args) {
